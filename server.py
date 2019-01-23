64e91a0feb22d0148987d4c9a63ed15fdfac965e
ybekele <yonael@ualberta.ca>
2019-01-22 22:24:11 -0700

    Finished handling 404 & 405 errors

--- server.py
+++ server.py
@@ -26,6 +26,25 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+# https://www.tutorialspoint.com/http/http_message_examples.htm
+# Formatting help
+# Client Request
+# GET /hello.htm HTTP/1.1
+# User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)
+# Host: www.tutorialspoint.com
+# Accept-Language: en-us
+# Accept-Encoding: gzip, deflate
+# Connection: Keep-Alive
+
+# Server response
+# HTTP/1.1 200 OK
+# Date: Mon, 27 Jul 2009 12:28:53 GMT
+# Server: Apache/2.2.14 (Win32)
+# Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT
+# Content-Length: 88
+# Content-Type: text/html
+# Connection: Closed
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
 
@@ -33,10 +52,82 @@ class MyWebServer(socketserver.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
 
-        request = self.data.split() # split the data request into an array of strings
-        print("this is the request " + request)
+        # split the data request into an array of strings
+        #http_method, http_path, http_version, http_host =
+        parsed_request = self.parse_request(self.data)
+
+        # Splits up the contents of the parse request by method, path and type 
+        if parsed_request != None:
+            http_method = parsed_request[0]
+            http_path = parsed_request[1]
+            http_type = parsed_request[2]
+
+
+            # Handles if the request is not a GET request
+            if http_method != "GET":
+                self.send_error(http_type, 405)
+                return
+
+            # Handles if it is a GET request
+            else:
+
+
+        # Handles if there is an error in parse_request
+        else:
+            raise AttributeError
+            return
+
+
+
+        # Uncomment this line to get contents of request. [method, path, hostID]
+        #Results were: [b'GET', b'/', b'HTTP/1.1', b'Host:', b'127.0.0.1:8080', b'User-Agent:', b'curl/7.54.0', b'Accept:', b'*/*']
+
+
 
         self.request.sendall(bytearray("OK",'utf-8'))
+
+#https://github.com/python/cpython/blob/master/Lib/http/server.py#L147
+    def parse_request(self, request):
+        # # splits the request on
+        # parsed_request = request.split()
+        # return parsed_request
+        parsed_request = request.split()
+
+        if len(parsed_request) == 0:
+            return False
+
+        # if len(parsed_request) >= 3:
+        #     print(parsed_request[2])
+        #     try:
+        #         print(parsed_request[2].Contains('HTTP/'))
+        #         if not parsed_request[2].Contains('HTTP/'):
+        #             raise ValueError
+        #             return
+        #     except:
+        #         print("Error parsing HTTP request")
+        #         return
+        print(parsed_request)
+        return parsed_request
+
+
+
+    def send_error(self, version, error_code):
+        # HTTP 1.1, 404
+
+        if error_code == 405:
+            output = (("%s %d %s\r\n" %
+                    (version, error_code, "Method Not Allowed")).encode(
+                        'latin-1', 'strict'))
+
+
+        if error_code == 404:
+            output = (("%s %d %s\r\n" %
+                    (version, error_code, "Not Found")).encode(
+                        'latin-1', 'strict'))
+
+        self.request.sendall(output)
+
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
